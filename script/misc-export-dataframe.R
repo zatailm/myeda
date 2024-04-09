c519522c3e8767431e0dffd1ba9a3bec7f748d2f
zata18 <inizata@gmail.com>
2024-04-09 19:52:49 +0800

    Initial commit

---
+++ script/misc-export-dataframe.R
@@ -0,0 +1,18 @@
+
+
+# export all data frame into file -------------------------------------------------------------
+
+all_objects <- ls()
+
+data_frames <- Filter(function(x) is.data.frame(get(x)), all_objects)
+
+save_data <- function(data_list) {
+  for (data_name in data_list) {
+    data_object <- get(data_name) 
+    file_name <- paste0("data/", data_name, ".rds")
+    saveRDS(data_object, file_name)
+  }
+}
+
+save_data(data_frames)
+
